9c3a6039a55e7c636f67854d5026aa11a2297148
heewookji <wlgmldnr@daum.net>
2020-07-26 17:24:44 +0900

    .

--- BOJ2557.java
+++ BOJ2557.java
@@ -1,7 +1,6 @@
-import java.util.*;
-
 class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("test");
 	}
 }
