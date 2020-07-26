391aa03952bd7ad0b0acfc858cd6b41001610336
JGeun <pjk8926@gmail.com>
2020-07-26 21:56:29 +0900

    delete folder

--- src/main/java/BOJ/JGeun/BOJ2557.java
+++ src/main/java/BOJ/JGeun/BOJ2557.java
@@ -2,8 +2,6 @@ package BOJ.JGeun;
 
 class BOJ2557 {
 	public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("test");
-    System.out.println("test2");
+		System.out.println("Hello World!");
 	}
 }
