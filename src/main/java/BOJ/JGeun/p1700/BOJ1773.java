4dc4bbba95138213c8d505f2d76e03ac5fb2cbbb
JGeun <68798525+JGeun@users.noreply.github.com>
2020-07-26 23:39:42 +0900

    폭죽쇼

---
+++ src/main/java/BOJ/JGeun/p1700/BOJ1773.java
@@ -0,0 +1,23 @@
+package BOJ.JGeun.p1700;
+
+class BOJ1700{
+  public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(), C=sc.nextInt();
+		boolean[] time = new boolean[C+1];
+		for(int i=0; i<N; i++) {
+			int t = sc.nextInt();
+			int j=1;
+			while(t*j<=C) {
+				time[t*j] = true;
+				j+=1;
+			}
+		}
+		int count =0;
+		for(int i=1; i<=C; i++) {
+			if(time[i])
+				count+=1;
+		}
+		System.out.println(count);
+	}
+}
