d739ac61a9f340b2d77c56eb583ada62fa728acc
JGeun <68798525+JGeun@users.noreply.github.com>
2020-07-27 18:14:55 +0900

    고양이

--- src/main/java/BOJ/JGeun/p10100/BOJ10171.java
+++ src/main/java/BOJ/JGeun/p10100/BOJ10171.java
@@ -2,9 +2,9 @@ package BOJ.JGeun.p10100;
 
 class BOJ10171{
     public static void main(String[] args) {
-		  System.out.println("\\    /\\");
-		  System.out.println(" )  ( ')");
-		  System.out.println("(  /  )");
-		  System.out.println(" \\(__)|");
-	}
+	System.out.println("\\    /\\");
+	System.out.println(" )  ( ')");
+	System.out.println("(  /  )");
+	System.out.println(" \\(__)|");
+   }
 }
