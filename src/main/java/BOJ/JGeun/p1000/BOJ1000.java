0a6fd457aff81c4f31af5a11f78f69320fc2e061
JGeun <68798525+JGeun@users.noreply.github.com>
2020-07-27 18:23:52 +0900

    A+B

--- src/main/java/BOJ/JGeun/p1000/BOJ1000.java
+++ src/main/java/BOJ/JGeun/p1000/BOJ1000.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 class BOJ1000{
   public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt(), b = sc.nextInt();
-		System.out.println(a + b);
+    	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt(), b = sc.nextInt();
+	System.out.println(a + b);
   }
 }
