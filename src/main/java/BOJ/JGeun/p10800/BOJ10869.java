207e5503a0e22d788d9a1bda1efac751b15a34ce
JGeun <pjk8926@gmail.com>
2020-07-29 03:41:53 +0900

    사칙연산

---
+++ src/main/java/BOJ/JGeun/p10800/BOJ10869.java
@@ -0,0 +1,15 @@
+package BOJ.JGeun.p10800;
+
+import java.util.Scanner;
+
+public class BOJ10869 {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt(), B= sc.nextInt();
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	}
+}
