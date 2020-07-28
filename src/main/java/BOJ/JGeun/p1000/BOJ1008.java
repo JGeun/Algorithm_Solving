c5d60b14d802c66d8d28bb9b76ff1ea024ba4133
JGeun <pjk8926@gmail.com>
2020-07-29 03:38:47 +0900

    A/B

---
+++ src/main/java/BOJ/JGeun/p1000/BOJ1008.java
@@ -0,0 +1,11 @@
+package BOJ.JGeun.p1000;
+
+import java.util.Scanner;
+
+public class BOJ1008 {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble(), b=sc.nextDouble();
+		System.out.println(a /b);
+	}
+}
