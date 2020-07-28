1210474e134faa6fd6b14e07ad9bc1b4789b1e6c
JGeun <pjk8926@gmail.com>
2020-07-29 06:42:40 +0900

    초콜릿 자르기

---
+++ src/main/java/BOJ/JGeun/p2100/BOJ2163.java
@@ -0,0 +1,11 @@
+package BOJ.JGeun.p2100;
+
+import java.util.Scanner;
+
+public class BOJ2163 {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(), M = sc.nextInt();
+		System.out.println(N*M-1);
+	}
+}
