deca56afd0b651acc2c92ae3009a1ab645683c64
JGeun <pjk8926@gmail.com>
2020-07-29 03:34:45 +0900

    A*B

---
+++ src/main/java/BOJ/JGeun/p10900/BOJ10998.java
@@ -0,0 +1,11 @@
+package BOJ.JGeun.p10900;
+
+import java.util.Scanner;
+
+public class BOJ10998 {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int A=sc.nextInt(), B=sc.nextInt();
+		System.out.println(A*B);
+	}
+}
