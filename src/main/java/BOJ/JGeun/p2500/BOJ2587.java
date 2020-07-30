6a36ffc7a5dc7f0fb95351667c89ba3222444c10
JGeun <pjk8926@gmail.com>
2020-07-30 17:04:25 +0900

    대표값2

---
+++ src/main/java/BOJ/JGeun/p2500/BOJ2587.java
@@ -0,0 +1,20 @@
+package BOJ.JGeun.p2500;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ2587 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] num = new int[5];
+		
+		int sum = 0;
+		for(int i=0; i<5; i++) {
+			num[i] = sc.nextInt();
+			sum += num[i];
+		}
+		Arrays.sort(num);
+		System.out.println(sum/5);
+		System.out.println(num[2]);
+	}
+}
