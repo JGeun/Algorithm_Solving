2d2e1b3ebd3249bd07aa2c0f7eb2e65b45adb3bd
JGeun <68798525+JGeun@users.noreply.github.com>
2020-07-31 13:40:18 +0900

    시험 성적

--- src/main/java/BOJ/JGeun/p9400/BOJ9498.java
+++ src/main/java/BOJ/JGeun/p9400/BOJ9498.java
@@ -1,5 +1,20 @@
 package BOJ.JGeun.p9400;
 
-public class BOJ9498 {
+import java.util.*;
 
+public class BOJ9498 {
+  public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int score = sc.nextInt();
+		if(score >= 90)
+			System.out.println("A");
+		else if(score >= 80)
+			System.out.println("B");
+		else if(score >= 70)
+			System.out.println("C");
+		else if(score >= 60)
+			System.out.println("D");
+		else
+			System.out.println("F");
+	}
 }
