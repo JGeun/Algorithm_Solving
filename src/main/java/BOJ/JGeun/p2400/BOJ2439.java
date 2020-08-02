c5c4eb401cc5d264af65fc53f25f3a64dedf0e55
JGeun <pjk8926@gmail.com>
2020-08-03 01:16:14 +0900

    별 찍기 -2

---
+++ src/main/java/BOJ/JGeun/p2400/BOJ2439.java
@@ -0,0 +1,17 @@
+package BOJ.JGeun.p2400;
+
+import java.util.Scanner;
+
+public class BOJ2439 {
+	public static void main(String[] args){ 
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N-i-1; j++)
+				System.out.print(" ");
+			for(int j=0; j<=i; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+}
