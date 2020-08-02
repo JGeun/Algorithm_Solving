89c10e915437ee343c71486b010fce2fc9e03501
JGeun <pjk8926@gmail.com>
2020-08-02 10:59:47 +0900

    A+B-4

---
+++ src/main/java/BOJ/JGeun/p10900/BOJ10951.java
@@ -0,0 +1,13 @@
+package BOJ.JGeun.p10900;
+
+import java.util.*;
+
+public class BOJ10951 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextInt()){
+			int A = sc.nextInt(), B = sc.nextInt();
+			System.out.println(A+B);
+		}
+	}
+}
