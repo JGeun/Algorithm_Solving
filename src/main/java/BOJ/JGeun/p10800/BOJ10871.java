8c6fc15591a40c5be70e24439cee6059c4755cd2
JGeun <pjk8926@gmail.com>
2020-08-05 00:31:28 +0900

    X보다 작은 수

---
+++ src/main/java/BOJ/JGeun/p10800/BOJ10871.java
@@ -0,0 +1,16 @@
+package BOJ.JGeun.p10800;
+
+import java.util.Scanner;
+
+public class BOJ10871 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(), X=sc.nextInt();
+		int[] array = new int[N];
+		for(int i=0; i<N; i++)
+			array[i] = sc.nextInt();
+		for(int i=0; i<N; i++)
+			if(array[i] < X)
+				System.out.print(array[i] + " ");
+	}
+}
