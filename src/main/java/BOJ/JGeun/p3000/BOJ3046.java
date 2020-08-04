84813e4b932320314feea3ad0dfb8e9378737349
JGeun <pjk8926@gmail.com>
2020-08-05 00:50:12 +0900

    R2

---
+++ src/main/java/BOJ/JGeun/p3000/BOJ3046.java
@@ -0,0 +1,12 @@
+package BOJ.JGeun.p3000;
+
+import java.util.Scanner;
+
+public class BOJ3046 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int R1 = sc.nextInt(), S=sc.nextInt();
+		System.out.println(2*S-R1);
+	}
+}
