63051c46d35cda18588d580d7cfd50b1fc42afcd
JGeun <pjk8926@gmail.com>
2020-08-05 00:21:21 +0900

    빠른 A+B

---
+++ src/main/java/BOJ/JGeun/p15500/BOJ15552.java
@@ -0,0 +1,19 @@
+package BOJ.JGeun.p15500;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ15552 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		for(int i=0; i<T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			bw.write((A+B)+"\n");
+		}
+		bw.flush();
+	}
+}
