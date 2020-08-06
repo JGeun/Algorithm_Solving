4751af67776a348169fbb75838351066e0139a2b
JGeun <pjk8926@gmail.com>
2020-08-06 13:58:56 +0900

    더하기 사이클

---
+++ src/main/java/BOJ/JGeun/p1100/BOJ1110.java
@@ -0,0 +1,18 @@
+package BOJ.JGeun.p1100;
+
+import java.util.Scanner;
+
+public class BOJ1110 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int count = 0;
+		int num = N;
+		do {
+			num = (num/10+num%10)%10+num%10*10;
+			count+=1;
+		}while(N != num);
+		System.out.println(count);
+	}
+}
