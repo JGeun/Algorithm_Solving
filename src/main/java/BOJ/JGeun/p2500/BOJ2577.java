a86de8aa1ca02ab61a7231a717d9f7a24d33190f
JGeun <pjk8926@gmail.com>
2020-08-07 09:53:39 +0900

    숫자의 개수

---
+++ src/main/java/BOJ/JGeun/p2500/BOJ2577.java
@@ -0,0 +1,16 @@
+package BOJ.JGeun.p2500;
+
+import java.util.Scanner;
+
+public class BOJ2577 {
+	public static void main(String[] args){
+		Scanner sc= new Scanner(System.in);
+		int A = sc.nextInt(), B=sc.nextInt(), C=sc.nextInt();
+		String number = Integer.toString(A*B*C);
+		int[] num = new int[10];
+		for(int i=0; i<number.length(); i++)
+			num[number.charAt(i)-'0']+=1;
+		for(int i=0; i<10; i++)
+			System.out.println(num[i]);
+	}
+}
