fef004e113ca892d6497d5e20c66550b86e63e83
JGeun <pjk8926@gmail.com>
2020-08-08 15:15:46 +0900

    이친수

---
+++ src/main/java/BOJ/JGeun/p2100/BOJ2193.java
@@ -0,0 +1,21 @@
+package BOJ.JGeun.p2100;
+
+import java.util.Scanner;
+
+public class BOJ2193 {
+	public static void main(String[] args){
+		Scanner sc= new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		if(N==1 || N==2)
+			System.out.println(1);
+		else {
+			long[] dp = new long[N+1];
+			dp[1] = 1;
+			dp[2] = 1;
+			for(int i=3; i<=N; i++)
+				dp[i] = dp[i-1] + dp[i-2];
+			System.out.println(dp[N]);
+		}
+	}
+}
