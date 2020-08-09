03bfe4f06d80a55c60bd406a97a369c51419698e
JGeun <pjk8926@gmail.com>
2020-08-09 13:50:44 +0900

    가장 큰 증가 부분 수열

---
+++ src/main/java/BOJ/JGeun/p11000/BOJ11055.java
@@ -0,0 +1,31 @@
+package BOJ.JGeun.p11000;
+
+import java.util.Scanner;
+
+public class BOJ11055 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] num = new int[N+1];
+		int[] dp = new int[N+1];
+		
+		for(int i=1; i<=N; i++)
+			num[i] = sc.nextInt();
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<i; j++) {
+				if(num[i] > num[j])
+					dp[i] = Math.max(dp[i], dp[j]);
+			}
+			dp[i] += num[i];
+		}
+		
+		int max = 0;
+		for(int i=1;i<=N; i++) {
+			if(max < dp[i])
+				max = dp[i];
+		}
+		System.out.println(max);
+	}
+}
