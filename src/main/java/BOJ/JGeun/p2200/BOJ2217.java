84a0b3b2734b118d90ac52dabe6014045f9e5207
JGeun <pjk8926@gmail.com>
2020-08-10 12:36:41 +0900

    로프

---
+++ src/main/java/BOJ/JGeun/p2200/BOJ2217.java
@@ -0,0 +1,22 @@
+package BOJ.JGeun.p2200;
+
+import java.util.*;
+
+public class BOJ2217 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int N =sc.nextInt();
+		int[] rope = new int[N];
+		for(int i=0; i<N; i++)
+			rope[i] =sc.nextInt();
+		
+		Arrays.sort(rope);
+		long max =0;
+		for(int i=0; i<N; i++) {
+			int n = rope[i]*(N-i);
+			if(max < n)
+				max = n;
+		}
+		System.out.println(max);
+	}
+}
