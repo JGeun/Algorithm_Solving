7972f05f7136c4e1a5868e4bcd00338265f594d0
JGeun <pjk8926@gmail.com>
2020-08-13 07:40:53 +0900

    상수

---
+++ src/main/java/BOJ/JGeun/p2900/BOJ2908.java
@@ -0,0 +1,23 @@
+package BOJ.JGeun.p2900;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ2908 {
+	public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	StringBuilder sb = new StringBuilder();
+    	
+    	int A = Integer.parseInt(sb.append(st.nextToken()).reverse().toString());
+    	sb.setLength(0);
+    	int B = Integer.parseInt(sb.append(st.nextToken()).reverse().toString());
+    	
+    	if(A > B)
+    		bw.append(A+"");
+    	else
+    		bw.append(B+"");
+    	bw.flush();
+    }
+}
