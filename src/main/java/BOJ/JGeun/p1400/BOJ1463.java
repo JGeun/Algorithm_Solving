07615e8c4dad02c75ad6b02d08a68e82f08a8f2f
JGeun <pjk8926@gmail.com>
2020-08-16 13:50:59 +0900

    1로 만들기

---
+++ src/main/java/BOJ/JGeun/p1400/BOJ1463.java
@@ -0,0 +1,32 @@
+package BOJ.JGeun.p1400;
+
+import java.util.Scanner;
+
+public class BOJ1463 {
+	static int n;
+	static int[] dp;
+	static void getCount(int num){
+		for(int i=2; i<=num; i++){
+			int min=1000000;
+			if(i%3 == 0){
+				if(min > dp[i/3])
+					min = dp[i/3]+1;
+			}
+			if(i%2==0){
+				if(min > dp[i/2])
+					min = dp[i/2]+1;
+			} 
+			if(min>dp[i-1])
+				min = dp[i-1]+1;
+			dp[i] = min;
+		}
+	}
+	public static void main (String[] args){
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		dp = new int[n+1];
+		dp[0] = 0; dp[1] = 0;
+		getCount(n);
+		System.out.println(dp[n]);
+	}
+}
