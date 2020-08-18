5d4cb1ccd99b095095bfa3a3e49db9057416f665
JGeun <pjk8926@gmail.com>
2020-08-18 13:49:32 +0900

    달팽이는 올라가고 싶ㄷ다

---
+++ src/main/java/BOJ/JGeun/p2800/BOJ2869.java
@@ -0,0 +1,15 @@
+package BOJ.JGeun.p2800;
+
+import java.util.Scanner;
+
+public class BOJ2869 {
+	static double A, B, V;
+	public static void main (String[] args){
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextDouble(); B =sc.nextDouble(); V = sc.nextDouble();
+		if(V == A)
+			System.out.println("1");
+		else
+			System.out.println( (long)Math.ceil((V-A)/(A-B))+1);
+	}
+}
