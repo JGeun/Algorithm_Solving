60c7fb0f9f4233343f09bcb393c465620771b768
JGeun <pjk8926@gmail.com>
2020-08-19 11:29:59 +0900

    ACM 호텔

---
+++ src/main/java/BOJ/JGeun/p10200/BOJ10250.java
@@ -0,0 +1,19 @@
+package BOJ.JGeun.p10200;
+
+import java.util.Scanner;
+
+public class BOJ10250 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for(int i=0; i<T; i++) {
+			int H = sc.nextInt(), W=sc.nextInt(), N= sc.nextInt();
+			int floor = N%H;
+			if(floor == 0)
+				floor = H;
+			int number = (N-1)/H+1;
+			System.out.println(floor*100+number);
+		}
+	}
+}
