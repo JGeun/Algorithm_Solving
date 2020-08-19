9858949aa0b4d852a2949be86c7613b3809d275e
heewookji <wlgmldnr@daum.net>
2020-08-20 00:03:30 +0900

    fix: 2775

--- src/main/java/BOJ/Heewook/mathBasic/Calculate2775.java
+++ src/main/java/BOJ/Heewook/mathBasic/Calculate2775.java
@@ -1,18 +1,15 @@
 package BOJ.Heewook.mathBasic;
-
 import java.util.Scanner;
-
 public class Calculate2775 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int c = sc.nextInt();
-    for (int i = 0; i < c; i++)
+    while (c-- > 0)
       System.out.println(find(sc.nextInt(), sc.nextInt()));
   }
-
   private static int find(int k, int n) {
     if (k == 0)
-      return n;
+      return 0;
     if (n == 0)
       return 0;
     return find(k, n - 1) + find(k - 1, n);
