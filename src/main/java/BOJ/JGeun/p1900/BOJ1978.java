d4ec9e6be29438ba096f5950132a1bd483c55f1d
JGeun <pjk8926@gmail.com>
2020-08-20 13:30:09 +0900

    소수 찾기

---
+++ src/main/java/BOJ/JGeun/p1900/BOJ1978.java
@@ -0,0 +1,33 @@
+package BOJ.JGeun.p1900;
+
+import java.util.Scanner;
+
+public class BOJ1978 {
+	static int N;
+	static int[] arr;
+	static boolean isPrime(int n){{
+		if(n==1) return false;
+		else{
+			for(int k=2; k<n; k++)
+				if(n%k==0)
+					return false;
+			}
+		}
+		return true;
+	}
+	static int getPrimeCount(int[] arr){
+		int count=0;
+		for(int i=0; i<arr.length; i++)
+			if(isPrime(arr[i]))
+				count+=1;
+		return count;
+	}
+	public static void main (String[] args){
+		Scanner sc = new Scanner(System.in);
+		N =sc.nextInt();
+		arr = new int[N];
+		for(int i=0; i<N; i++)
+			arr[i] = sc.nextInt();
+		System.out.println(getPrimeCount(arr));
+	}
+}
