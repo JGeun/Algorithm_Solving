a419d386fb5c1b35c96dd3a66b5dcb5d9cefc38b
heewookji <wlgmldnr@daum.net>
2020-08-24 14:34:10 +0900

    feat: 1085

---
+++ src/main/java/BOJ/Heewook/mathBasic/Square.java
@@ -0,0 +1,20 @@
+package BOJ.Heewook.mathBasic;
+
+import java.util.Scanner;
+
+public class Square {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int x = scanner.nextInt();
+    int y = scanner.nextInt();
+    int w = scanner.nextInt();
+    int h = scanner.nextInt();
+    int xDiff = Math.min(w - x, x);
+    int yDiff = Math.min(h - y, y);
+    System.out.println(Math.min(xDiff, yDiff));
+
+  }
+
+}
