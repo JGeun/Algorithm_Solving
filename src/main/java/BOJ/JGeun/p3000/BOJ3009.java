b913e97055a1db7fcd86cfc410e16898cca8b447
JGeun <pjk8926@gmail.com>
2020-08-25 14:54:22 +0900

    네 번째 점

---
+++ src/main/java/BOJ/JGeun/p3000/BOJ3009.java
@@ -0,0 +1,23 @@
+package BOJ.JGeun.p3000;
+
+import java.util.Scanner;
+
+public class BOJ3009 {
+	static int findNotEqual(int n1, int n2, int n3){
+		if(n1 != n2){
+			if(n1 == n3)
+				return n2;
+			else
+				return n1;
+		}
+		return n3;
+	}
+	public static void main (String[] args){
+		Scanner sc = new Scanner(System.in);
+		int x1=sc.nextInt(), y1=sc.nextInt();
+		int x2=sc.nextInt(), y2=sc.nextInt();
+		int x3=sc.nextInt(), y3=sc.nextInt();
+		
+		System.out.println(findNotEqual(x1,x2,x3)+" "+findNotEqual(y1,y2,y3));
+	}
+}
