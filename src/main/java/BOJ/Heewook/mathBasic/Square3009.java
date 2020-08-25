b832a649b9beb4f47b77695a27e8cdd19aca8719
heewookji <wlgmldnr@daum.net>
2020-08-25 18:55:49 +0900

    feat: 3009

--- src/main/java/BOJ/Heewook/mathBasic/Square3009.java
+++ src/main/java/BOJ/Heewook/mathBasic/Square3009.java
@@ -1,7 +1,5 @@
 package BOJ.Heewook.mathBasic;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Square3009 {
@@ -9,23 +7,14 @@ public class Square3009 {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
-    Map<Integer, Integer> xMap = new HashMap<>();
-    Map<Integer, Integer> yMap = new HashMap<>();
-    StringBuilder builder = new StringBuilder();
-
-    for (int i = 0; i < 3; i++) {
-      int x = scanner.nextInt();
-      int y = scanner.nextInt();
-      xMap.put(x, xMap.getOrDefault(x, 0) + 1);
-      yMap.put(y, yMap.getOrDefault(y, 0) + 1);
-    }
-    for (Integer key : xMap.keySet())
-      if (xMap.get(key) % 2 != 0)
-        builder.append(key + " ");
-    for (Integer key : yMap.keySet())
-      if (yMap.get(key) % 2 != 0)
-        builder.append(key);
-    System.out.println(builder);
+    int x1 = scanner.nextInt();
+    int y1 = scanner.nextInt();
+    int x2 = scanner.nextInt();
+    int y2 = scanner.nextInt();
+    int x3 = scanner.nextInt();
+    int y3 = scanner.nextInt();
+    System.out.println((x1 ^ x2 ^ x3) + " " + (y1 ^ y2 ^ y3));
+    
   }
 
 }
