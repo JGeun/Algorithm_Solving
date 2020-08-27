2ec54a63efcc7d7803a0e47a31352bc23fdcddd8
heewookji <wlgmldnr@daum.net>
2020-08-27 19:22:21 +0900

    feat: 10872

---
+++ src/main/java/BOJ/Heewook/recursive/Factorial.java
@@ -0,0 +1,19 @@
+package BOJ.Heewook.recursive;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    System.out.println(recursive(n));
+  }
+
+  private static int recursive(int n) {
+    if (n == 0)
+      return 1;
+    return n * recursive(n - 1);
+  }
+
+}
