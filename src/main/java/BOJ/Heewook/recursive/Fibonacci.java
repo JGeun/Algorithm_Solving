387044afbf2f14b6c3765c3950235b1f4312db8a
heewookji <wlgmldnr@daum.net>
2020-08-27 20:30:49 +0900

    feat: 10870

--- src/main/java/BOJ/Heewook/recursive/Fibonacci.java
+++ src/main/java/BOJ/Heewook/recursive/Fibonacci.java
@@ -7,13 +7,34 @@ public class Fibonacci {
 
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
-    System.out.println(recursive(n));
+    System.out.println("recursive " + recursive(n));
+    System.out.println("tailRecursive " + tailRecursive(n, 1, 0));
+    System.out.println("loop " + loop(n));
+  }
+
+  private static int tailRecursive(int n, int pre, int prepre) {
+    if (n < 2)
+      return n * pre;
+    return tailRecursive(n - 1, pre + prepre, pre);
   }
 
   private static int recursive(int n) {
     if (n == 0 || n == 1)
       return n;
     return recursive(n - 1) + recursive(n - 2);
+  }
+
+  private static int loop(int n) {
+
+    int prepre = 0;
+    int pre = 1;
+    int current = 1;
+    for (int i = 2; i <= n; i++) {
+      current = pre + prepre;
+      prepre = pre;
+      pre = current;
+    }
+    return current;
   }
 
 }
