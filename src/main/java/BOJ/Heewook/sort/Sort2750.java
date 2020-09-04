0b86668178afd2d5439a685ebc83f04848c89733
heewookji <wlgmldnr@daum.net>
2020-09-04 20:48:58 +0900

    feat: 2750

--- src/main/java/BOJ/Heewook/sort/Sort2750.java
+++ src/main/java/BOJ/Heewook/sort/Sort2750.java
@@ -1,19 +1,33 @@
 package BOJ.Heewook.sort;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Sort2750 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
-    int[] num = new int[n];
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int n = Integer.valueOf(br.readLine());
+    ArrayList<Integer> num = new ArrayList<>();
     for (int i = 0; i < n; i++)
-      num[i] = scanner.nextInt();
+      num.add(Integer.valueOf(br.readLine()));
+    sort2(num);
+    for (int i = 0; i < n; i++)
+      bw.write(num.get(i) + "\n");
+    bw.flush();
+  }
+
+  private static void sort1(final int[] num) {
     int tmp = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
+    for (int i = 0; i < num.length; i++) {
+      for (int j = i + 1; j < num.length; j++) {
         if (num[i] > num[j]) {
           tmp = num[i];
           num[i] = num[j];
@@ -21,8 +35,10 @@ public class Sort2750 {
         }
       }
     }
-    for (int i = 0; i < n; i++)
-      System.out.println(num[i]);
+  }
+
+  private static void sort2(ArrayList<Integer> num) {
+    Collections.sort(num);
   }
 
 }
