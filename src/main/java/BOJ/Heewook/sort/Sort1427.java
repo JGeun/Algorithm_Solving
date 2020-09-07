581bab95c83dcc5e87ae6267cc031edc09ea08b4
Heewookji <wlgmldnr@daum.net>
2020-09-07 13:44:07 +0000

    feat: 1427

--- src/main/java/BOJ/Heewook/sort/Sort1427.java
+++ src/main/java/BOJ/Heewook/sort/Sort1427.java
@@ -5,21 +5,21 @@ import java.util.*;
 
 public class Sort1427 {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-		int num = Integer.valueOf(scanner.nextInt());
-		ArrayList<Integer> list = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    int num = Integer.valueOf(scanner.nextInt());
+    ArrayList<Integer> list = new ArrayList<>();
 
-		while (num > 0){
-			list.add(num % 10);
-			num /= 10;
-		}
+    while (num > 0){
+      list.add(num % 10);
+      num /= 10;
+    }
 
-		Collections.sort(list, Collections.reverseOrder());
+    Collections.sort(list, Collections.reverseOrder());
  
-		for (int i : list) 
-			System.out.print(i);
-	}
+    for (int i : list) 
+      System.out.print(i);
+  }
 
 }
