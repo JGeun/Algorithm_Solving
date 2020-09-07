549e03cf1fdb1f8bbeac6f67cafb77526240a907
JGeun <pjk8926@gmail.com>
2020-09-07 13:22:11 +0900

    최대 힙

---
+++ src/main/java/BOJ/JGeun/p11200/BOJ11279.java
@@ -0,0 +1,18 @@
+package BOJ.JGeun.p11200;
+
+import java.util.*;
+
+public class BOJ11279 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for(int i=0; i<n; i++) {
+			int num = sc.nextInt();
+			pq.add(num);
+			if(num == 0)
+				System.out.println(pq.poll());
+		}
+	}
+}
