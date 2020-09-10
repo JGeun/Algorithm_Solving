63a9910b1972fee29d2b758d467a7fdfd05f49b9
Heewookji <wlgmldnr@daum.net>
2020-09-10 11:36:46 +0000

    feat: 1181

--- src/main/java/BOJ/Heewook/sort/Sort1181.java
+++ src/main/java/BOJ/Heewook/sort/Sort1181.java
@@ -10,8 +10,8 @@ public class Sort1181 {
 
   public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
     Set<String> list = new TreeSet<>((s1, s2) -> {
       if (s1.length() != s2.length())
         return s1.length() - s2.length();
@@ -19,7 +19,7 @@ public class Sort1181 {
         return s1.compareTo(s2);
     });
     for (int i = 0; i < n; i++)
-      list.add(scanner.next());
+      list.add(sc.next());
     for (String s : list)
       System.out.println(s);
   }
