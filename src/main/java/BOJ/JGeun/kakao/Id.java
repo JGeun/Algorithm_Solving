ae78faefbfcaf0907d7934d13abfc8e786e08ad1
JGeun <pjk8926@gmail.com>
2020-09-16 16:55:52 +0900

    kakao id

---
+++ src/main/java/BOJ/JGeun/kakao/Id.java
@@ -0,0 +1,54 @@
+package BOJ.JGeun.kakao;
+
+public class Id {
+	public static void main(String[] args) {
+		System.out.println(new Id().solution("..eeqweweadd.12.."));
+	}
+	public String solution(String new_id) {
+        String answer = getId(new_id);
+        return answer;
+    }
+    private String getId(String new_id){
+        //1단계
+        new_id = new_id.toLowerCase(); 
+        
+        //2단계, 3단계
+        StringBuilder sb = new StringBuilder();
+        boolean isGetForeDot = false;
+        for(int i=0; i<new_id.length(); i++){
+            char ch = new_id.charAt(i);
+            if((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <='z') || ch =='_' || ch == '-'){
+                sb.append(new_id.substring(i,i+1));
+                isGetForeDot = false;
+            }
+            else if(ch == '.' && !isGetForeDot){
+                sb.append(new_id.substring(i,i+1));
+                isGetForeDot = true;
+            }
+        }
+        new_id = sb.toString();
+        
+        //4단계
+        if(new_id.charAt(0) == '.')
+            new_id = new_id.substring(1, new_id.length());
+        else if(new_id.charAt(new_id.length()-1) == '.')
+            new_id = new_id.substring(0, new_id.length()-1);
+        
+        //5단계
+        if(new_id.equals(""))
+            new_id += "a";
+        
+        //6단계
+        if(new_id.length() >= 16)
+            new_id = new_id.substring(0, 15);
+        if(new_id.charAt(new_id.length()-1) == '.')
+            new_id = new_id.substring(0, new_id.length()-1);
+        
+        //7단계
+        String last = new_id.substring(new_id.length()-1,new_id.length());
+        while(new_id.length() <= 2){
+            new_id += last;
+        }
+        return new_id;
+    }
+}
