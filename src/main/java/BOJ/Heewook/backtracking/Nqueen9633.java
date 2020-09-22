53b6040e14eaf9d926ef39020894a02e22891616
heewookji <wlgmldnr@daum.net>
2020-09-23 00:04:52 +0900

    feat: 9633

--- src/main/java/BOJ/Heewook/backtracking/Nqueen9633.java
+++ src/main/java/BOJ/Heewook/backtracking/Nqueen9633.java
@@ -4,43 +4,35 @@ import java.util.Scanner;
 
 public class Nqueen9633 {
 
-  final static int[][] d = { { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 }, { -1, 0 } };
+  static int n;
 
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
-    int[][] board = new int[n][n];
-    System.out.println(find(board, n));
+    n = scanner.nextInt();
+    int[] arr = new int[n];
+    System.out.println(find(0, arr));
   }
 
-  private static int find(int[][] board, int n) {
-    if (n == 0)
+  private static int find(int y, int[] arr) {
+    if (y == n)
       return 1;
     int count = 0;
-    for (int x = 0; x < board.length; x++) {
-      if (board[n - 1][x] == 0) {
-        fill(x, n - 1, board, 1);
-        count += find(board, n - 1);
-        fill(x, n - 1, board, -1);
-      }
+    for (int x = 0; x < n; x++) {
+      arr[y] = x;
+      if (!isCorrect(x, y, arr))
+        continue;
+      count += find(y + 1, arr);
     }
     return count;
   }
 
-  private static void fill(int x, int y, int[][] board, int add) {
-    board[y][x] += add;
-    for (int i = 0; i < d.length; i++) {
-      int nextX = x;
-      int nextY = y;
-      for (int n = 0; n < board.length; n++) {
-        nextX += d[i][0];
-        nextY += d[i][1];
-        if (nextX < 0 || nextX >= board.length || nextY < 0 || nextY >= board.length)
-          break;
-        board[nextY][nextX] += add;
-      }
+  private static boolean isCorrect(int x, int y, int[] arr) {
+    for (int pastY = 0; pastY < y; pastY++) {
+      int pastX = arr[pastY];
+      if (pastX == x || Math.abs(x - pastX) == y - pastY)
+        return false;
     }
+    return true;
   }
-
 }
