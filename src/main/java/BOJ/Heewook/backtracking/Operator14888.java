568e61451e24d078b037f8e75e8e0cfc28d5b784
heewookji <wlgmldnr@daum.net>
2020-09-24 13:00:32 +0900

    feat: 14888

--- src/main/java/BOJ/Heewook/backtracking/Operator14888.java
+++ src/main/java/BOJ/Heewook/backtracking/Operator14888.java
@@ -1,6 +1,5 @@
 package BOJ.Heewook.backtracking;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Operator14888 {
@@ -13,56 +12,41 @@ public class Operator14888 {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
     int[] numbers = new int[n];
+    int[] operators = new int[4];
     for (int i = 0; i < n; i++)
       numbers[i] = scanner.nextInt();
-    ArrayList<Character> operators = new ArrayList<>();
-    for (int order = 0; order < 4; order++) {
-      int operatorN = scanner.nextInt();
-      for (int i = 0; i < operatorN; i++)
-        switch (order) {
-          case 0:
-            operators.add('+');
-            break;
-          case 1:
-            operators.add('-');
-            break;
-          case 2:
-            operators.add('*');
-            break;
-          case 3:
-            operators.add('/');
-        }
-    }
-    find(0, numbers[0], numbers, operators, new boolean[n - 1]);
+    for (int i = 0; i < 4; i++)
+      operators[i] = scanner.nextInt();
+    find(0, numbers[0], numbers, operators);
     System.out.println(max);
     System.out.println(min);
     scanner.close();
   }
 
-  private static void find(int n, int now, final int[] numbers, ArrayList<Character> operators, boolean[] used) {
+  private static void find(int n, int now, final int[] numbers, int[] operators) {
     if (n == numbers.length - 1) {
       max = Math.max(max, now);
       min = Math.min(min, now);
       return;
     }
-    for (int i = 0; i < operators.size(); i++) {
-      if (used[i])
-        continue;
-      used[i] = true;
-      switch (operators.get(i)) {
-        case '+':
-          find(n + 1, now + numbers[n + 1], numbers, operators, used);
-          break;
-        case '-':
-          find(n + 1, now - numbers[n + 1], numbers, operators, used);
-          break;
-        case '*':
-          find(n + 1, now * numbers[n + 1], numbers, operators, used);
-          break;
-        case '/':
-          find(n + 1, now / numbers[n + 1], numbers, operators, used);
+    for (int i = 0; i < 4; i++) {
+      if (operators[i] > 0) {
+        operators[i]--;
+        switch (i) {
+          case 0:
+            find(n + 1, now + numbers[n + 1], numbers, operators);
+            break;
+          case 1:
+            find(n + 1, now - numbers[n + 1], numbers, operators);
+            break;
+          case 2:
+            find(n + 1, now * numbers[n + 1], numbers, operators);
+            break;
+          case 3:
+            find(n + 1, now / numbers[n + 1], numbers, operators);
+        }
+        operators[i]++;
       }
-      used[i] = false;
     }
   }
 
