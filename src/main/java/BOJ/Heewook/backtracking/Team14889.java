880a2b3216cf2ebe70f8ffe5df52d0a3ac9f4386
heewookji <wlgmldnr@daum.net>
2020-09-29 02:12:31 +0900

    feat: 14889

--- src/main/java/BOJ/Heewook/backtracking/Team14889.java
+++ src/main/java/BOJ/Heewook/backtracking/Team14889.java
@@ -1,6 +1,5 @@
 package BOJ.Heewook.backtracking;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Team14889 {
@@ -10,48 +9,43 @@ public class Team14889 {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
     int[][] info = new int[n][n];
-    ArrayList<Integer> used = new ArrayList<>();
+    boolean[] used = new boolean[n];
 
     for (int i = 0; i < n; i++)
       for (int j = 0; j < n; j++)
         info[i][j] = scanner.nextInt();
 
-    recursive(n, info, used);
+    System.out.println(recursive(n / 2, 0, info, used));
   }
 
-  private static int recursive(int n, final int[][] info, ArrayList<Integer> used) {
+  private static int recursive(int n, int index, final int[][] info, boolean[] used) {
 
     if (n == 0)
       return calc(info, used);
 
     int ret = Integer.MAX_VALUE;
-    for (int i = 0; i < info.length; i++) {
-      if (used.contains(i))
+    for (int i = index; i < info.length; i++) {
+      if (used[i])
         continue;
-      used.add(i);
-      ret = Math.min(ret, recursive(n - 1, info, used));
-      used.remove(used.size() - 1);
+      used[i] = true;
+      ret = Math.min(ret, recursive(n - 1, i + 1, info, used));
+      used[i] = false;
     }
     return ret;
   }
 
-  private static int calc(final int[][] info, ArrayList<Integer> used) {
+  private static int calc(final int[][] info, boolean[] used) {
+    int sum = 0;
+    int sum2 = 0;
 
-    for (int i = 0; i < used.size(); i++) {
-      if (i == used.size() / 2)
-        System.out.println("==========");
-      System.out.println(used.get(i));
-    }
-    int a = 0;
-    int b = 0;
-    for (int i = 0; i < used.size(); i++) {
-      if (i >= used.size() / 2)
-        a += used.get(i);
-      else
-        b += used.get(i);
-    }
-    System.out.println("=============================================" + Math.abs(a - b));
-    return Math.abs(a - b);
+    for (int i = 0; i < used.length - 1; i++)
+      for (int j = i + 1; j < used.length; j++)
+        if (used[j] && used[i])
+          sum += info[i][j] + info[j][i];
+        else if (!used[j] && !used[i])
+          sum2 += info[i][j] + info[j][i];
+
+    return Math.abs(sum - sum2);
   }
 
 }
