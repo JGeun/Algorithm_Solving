ac8d4bf5ad42833de9a0123b35033682b6eaba83
heewookji <wlgmldnr@daum.net>
2020-10-30 20:02:04 +0900

    fix: 1003

--- src/main/java/BOJ/Heewook/dynamic/Fibonacci1003.java
+++ src/main/java/BOJ/Heewook/dynamic/Fibonacci1003.java
@@ -5,18 +5,21 @@ import java.util.Scanner;
 public class Fibonacci1003 {
 
   public static void main(String[] args) {
-
     Scanner scanner = new Scanner(System.in);
     int testN = scanner.nextInt();
+    int[] dp = new int[41];
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int j = 2; j <= 40; j++)
+      dp[j] = dp[j - 1] + dp[j - 2];
     for (int i = 0; i < testN; i++) {
       int n = scanner.nextInt();
-      int a = 1, b = 0;
-      while (n-- != 0) {
-        int tmp = a + b;
-        a = b;
-        b = tmp;
-      }
-      System.out.println(a + " " + b);
+      String ret;
+      if (n == 0)
+        ret = String.format("%d %d", 1, 0);
+      else
+        ret = String.format("%d %d", dp[n - 1], dp[n]);
+      System.out.println(ret);
     }
   }
 }
