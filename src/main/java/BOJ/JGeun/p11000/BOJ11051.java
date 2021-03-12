fde8defbabcb84523191243656a13e33767d64db
JGeun <pjk8926@gmail.com>
2021-03-12 14:01:59 +0900

    이항계수2

--- src/main/java/BOJ/JGeun/p11000/BOJ11051.java
+++ src/main/java/BOJ/JGeun/p11000/BOJ11051.java
@@ -1,4 +1,28 @@
 package BOJ.JGeun.p11000;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BOJ11051 {
+    static int[][] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N =  sc.nextInt();
+        int K = sc.nextInt();
+        dp = new int[N + 1][K + 1];
+        for(int i=0; i<N+1; i++)
+            Arrays.fill(dp[i], 0);
+
+        System.out.println(getBC(N, K));
+    }
+
+    public static int getBC(int N, int K){
+        if(dp[N][K] > 0)
+            return dp[N][K];
+        else if(N == K || K == 0)
+            return dp[N][K] = 1;
+        else
+            return dp[N][K] = (getBC(N-1, K-1) + getBC(N-1,K)) % 10007;
+    }
 }
