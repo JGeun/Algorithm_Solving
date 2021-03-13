c37d4fe2066780ceee70bbd1c0d41b9fa62bce1d
JGeun <pjk8926@gmail.com>
2021-03-13 14:28:02 +0900

    검문

--- src/main/java/BOJ/JGeun/p2900/BOJ2981.java
+++ src/main/java/BOJ/JGeun/p2900/BOJ2981.java
@@ -4,19 +4,35 @@ import java.io.*;
 import java.util.Arrays;
 
 public class BOJ2981{
+    public static int getGCD(int a, int b){
+        while(b != 0){
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+
+    public static void printDivisor(int n){
+        for(int i=2; i<=n; i++){
+            if(n%i == 0)
+                System.out.print(i+" ");
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        int max = 0;
-        for(int i=0; i<N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            arr[i] = num;
-        }
+
+        for(int i=0; i<N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+
         Arrays.sort(arr);
 
+        int gcd = arr[1]-arr[0];
+        for(int i=2; i<arr.length; i++)
+            gcd = getGCD(gcd, arr[i]-arr[i-1]);
+
+        printDivisor(gcd);
     }
 }
