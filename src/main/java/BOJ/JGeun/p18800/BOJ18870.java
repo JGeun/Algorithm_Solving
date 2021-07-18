5b94dda71bc5947f3f8f717efdde29e27418f0e4
JGeun <pjk8926@gmail.com>
2021-07-19 00:04:34 +0900

    -ing

---
+++ src/main/java/BOJ/JGeun/p18800/BOJ18870.java
@@ -0,0 +1,61 @@
+package BOJ.JGeun.p18800;
+
+import java.util.*;
+
+public class BOJ18870 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        QuickSorter.quickSort(arr);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<arr.length; i++){
+            if(!list.contains(arr[i]))
+                list.add(arr[i]);
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(list.indexOf(arr[i])+" ");
+        }
+    }
+}
+
+class QuickSorter {
+    public static void quickSort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int low, int high) {
+        if (low >= high) return;
+
+        int mid = partition(arr, low, high);
+        sort(arr, low, mid - 1);
+        sort(arr, mid, high);
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[(low + high) / 2];
+        while (low <= high) {
+            while (arr[low] < pivot) low++;
+            while (arr[high] > pivot) high--;
+            if (low <= high) {
+                swap(arr, low, high);
+                low++;
+                high--;
+            }
+        }
+        return low;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
