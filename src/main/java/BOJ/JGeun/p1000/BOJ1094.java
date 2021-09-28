a89404ae98e8beef62c077d5aa397bd2a83cd138
JGeun <pjk8926@gmail.com>
2021-09-28 11:33:45 +0900

    막대기

---
+++ src/main/java/BOJ/JGeun/p1000/BOJ1094.java
@@ -0,0 +1,35 @@
+package BOJ.JGeun.p1000;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ1094 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder("");
+
+    static int X;
+
+    static void input() throws IOException {
+        X = Integer.parseInt(br.readLine());
+    }
+
+    static void process() {
+        int ans = 0;
+        int stick = 64;
+
+        while(X >0){
+            if(stick > X) stick /= 2;
+            else{
+                ans+=1;
+                X-=stick;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        process();
+    }
+}
