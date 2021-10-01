6cb4b2485dfff439773f65dd096a9afc60b3b743
JGeun <pjk8926@gmail.com>
2021-10-02 01:17:12 +0900

    문자열 집합

---
+++ src/main/java/BOJ/JGeun/p14400/BOJ14425.java
@@ -0,0 +1,33 @@
+package BOJ.JGeun.p14400;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ14425 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder("");
+
+    static int N, M;
+    static HashMap<String, Integer> map = new HashMap<>();
+
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++) map.put(br.readLine(), 1);
+    }
+
+    static void process() throws IOException{
+        int ans = 0;
+        for(int i=0; i<M; i++){
+            if(map.get(br.readLine()) != null) ans+=1;
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        process();
+    }
+}
