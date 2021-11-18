d395ee23d095f6e5ef5ad13a5a8743cf3986b2b5
JGeun <pjk8926@gmail.com>
2021-11-19 00:17:43 +0900

    뒤집기

---
+++ src/main/java/BOJ/JGeun/p1400/BOJ1439.java
@@ -0,0 +1,42 @@
+package BOJ.JGeun.p1400;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1439 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder("");
+
+    static String S;
+
+    static void input() throws IOException {
+        S = br.readLine();
+    }
+
+    static void process(){
+        System.out.println(Math.min(getTypeCnt('0'), getTypeCnt('1')));
+    }
+
+    static int getTypeCnt(char type){
+        boolean isSame = false;
+        int cnt = 0;
+
+        for(int i=0; i<S.length(); i++){
+            if(S.charAt(i) == type){
+                if(!isSame){
+                    isSame = true;
+                    cnt +=1;
+                }
+            }else{
+                isSame = false;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) throws IOException{
+        input();
+        process();
+    }
+}
