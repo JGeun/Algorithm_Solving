58acd6ecaff986f5031f2a5eac0937267adcd966
JGeun <pjk8926@gmail.com>
2022-01-25 15:15:10 +0900

    직사각형 별찍기

---
+++ src/main/java/Programmers/level1/직사각형 별찍기.java
@@ -0,0 +1,16 @@
+package Programmers.level1;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        for(int i=0; i<b; i++){
+            for(int j=0; j<a; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
