00b159515918b7a42bb4820e8640cc7a818bf249
JGeun <pjk8926@gmail.com>
2022-01-27 21:00:19 +0900

    행렬의 덧셈

---
+++ src/main/java/Programmers/level1/행렬의 덧셈.java
@@ -0,0 +1,11 @@
+package Programmers.level1;
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = arr1;
+        for(int i=0; i<arr1.length; i++)
+            for(int j=0; j<arr1[0].length; j++)
+                answer[i][j] += arr2[i][j];
+        return answer;
+    }
+}
