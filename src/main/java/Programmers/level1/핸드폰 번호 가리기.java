30a00bbc7e5d9c8c168e7ba9bfe6b4d7cc8fa783
JGeun <pjk8926@gmail.com>
2022-01-28 00:20:50 +0900

    핸드폰 번호 가리기

---
+++ src/main/java/Programmers/level1/핸드폰 번호 가리기.java
@@ -0,0 +1,11 @@
+package Programmers.level1;
+
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        for(int i=0; i<phone_number.length()-4; i++)
+            answer = answer.concat("*");
+        answer = answer.concat(phone_number.substring(phone_number.length()-4, phone_number.length()));
+        return answer;
+    }
+}
