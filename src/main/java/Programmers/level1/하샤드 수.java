d3c0feb7a084e7a4419d2e922579cdd1a75bfe4a
JGeun <pjk8926@gmail.com>
2022-01-28 14:35:47 +0900

    하샤드 수

---
+++ src/main/java/Programmers/level1/하샤드 수.java
@@ -0,0 +1,15 @@
+package Programmers.level1;
+
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String num = Integer.toString(x);
+        int sum = 0;
+        for(int i=0; i<num.length(); i++)
+            sum += (num.charAt(i) -'0');
+
+        if(x%sum != 0)
+            answer =false;
+        return answer;
+    }
+}
