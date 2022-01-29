18a4f827805dca06956e9463b73164faa229eaaa
JGeun <pjk8926@gmail.com>
2022-01-29 22:32:23 +0900

    ??!

---
+++ src/main/java/BOJ/p10900/BOJ10926.java
@@ -0,0 +1,10 @@
+package BOJ.p10900;
+
+import java.util.Scanner;
+
+public class BOJ10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next()+"??!");
+    }
+}
