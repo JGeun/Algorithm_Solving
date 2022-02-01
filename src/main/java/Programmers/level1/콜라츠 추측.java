ed5d8ec4933d3ceb2a47f85b1bd6f6a74c0736ea
JGeun <pjk8926@gmail.com>
2022-02-01 12:02:51 +0900

    콜라츠 추측

---
+++ src/main/java/Programmers/level1/콜라츠 추측.java
@@ -0,0 +1,14 @@
+package Programmers.level1;
+
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long result = (long)num;
+        while(answer <= 500){
+            if(result == 1) return answer;
+            result = (result%2==0) ? result/=2 : result*3+1;
+            answer+=1;
+        }
+        return -1;
+    }
+}
