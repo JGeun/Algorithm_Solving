1dde4121a875d01e9ca21ef6287361bb2fdab10d
JGeun <pjk8926@gmail.com>
2022-02-04 12:11:56 +0900

    제일 작은 수 제거하기

---
+++ src/main/java/Programmers/level1/제일 작은 수 제거하기.java
@@ -0,0 +1,11 @@
+package Programmers.level1;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length <= 1) return new int[]{-1};
+        int min = Arrays.stream(arr).min().getAsInt();
+        return Arrays.stream(arr).filter(i -> i != min).toArray();
+    }
+}
