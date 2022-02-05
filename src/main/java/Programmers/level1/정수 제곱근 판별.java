e1d9aff36fe0512f01d70fb1da21cc2ce0e28397
JGeun <pjk8926@gmail.com>
2022-02-05 17:00:06 +0900

    정수 제곱근 판별

---
+++ src/main/java/Programmers/level1/정수 제곱근 판별.java
@@ -0,0 +1,11 @@
+package Programmers.level1;
+
+class Solution {
+    public long solution(long n) {
+        double num = Math.sqrt(n);
+        if(num == (int)num)
+            return (long)Math.pow((int)num+1 ,2);
+        else
+            return -1;
+    }
+}
