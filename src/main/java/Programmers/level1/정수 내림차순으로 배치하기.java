0b6811926999b3985de9cddb286487b01b70fd01
JGeun <pjk8926@gmail.com>
2022-02-06 13:10:42 +0900

    정수 내림차순으로 배치하기

---
+++ src/main/java/Programmers/level1/정수 내림차순으로 배치하기.java
@@ -0,0 +1,27 @@
+package Programmers.level1;
+
+import java.util.*;
+import java.util.stream.*;
+
+//Stream 활용
+class Solution {
+    public long solution(long n) {
+        return Long.parseLong(Arrays.stream(String.valueOf(n).split("")).sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining()));
+    }
+}
+
+//Collections.reverse활용
+class Solution {
+    public String reverseStr(String str){
+        String[] array = str.split("");
+        Arrays.sort(array);
+        Collections.reverse(Arrays.asList(array));
+        return String.join("", array);
+    }
+    public long solution(long n) {
+        String s = Long.toString(n);
+        Long answer = Long.parseLong(reverseStr(s));
+        return answer;
+    }
+}
