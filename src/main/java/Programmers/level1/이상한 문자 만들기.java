9e7cbda91606f06f63c826f68caf4f9fe410dd76
JGeun <pjk8926@gmail.com>
2022-02-09 01:27:28 +0900

    이상한 문자 만들기

---
+++ src/main/java/Programmers/level1/이상한 문자 만들기.java
@@ -0,0 +1,15 @@
+package Programmers.level1;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] words = s.split("");
+
+        int index = -1;
+        for(String word : words){
+            index = word.contains(" ") ? -1 : index + 1;
+            answer += index%2 == 0 ? word.toUpperCase() : word.toLowerCase();
+        }
+        return answer;
+    }
+}
