f131db9f95a3cf42e5775dac847f4c9c9108304f
JGeun <pjk8926@gmail.com>
2022-02-11 00:57:40 +0900

    시저 암호

---
+++ src/main/java/Programmers/level1/시저 암호.java
@@ -0,0 +1,18 @@
+package Programmers.level1;
+
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(Character.isLowerCase(ch)){ //a
+                sb.append((char)((ch-'a'+n)%26 + 'a'));
+            }else if(Character.isUpperCase(ch)){ //A
+                sb.append((char)((ch-'A'+n)%26 + 'A'));
+            }else{
+                sb.append(' ');
+            }
+        }
+        return sb.toString();
+    }
+}
