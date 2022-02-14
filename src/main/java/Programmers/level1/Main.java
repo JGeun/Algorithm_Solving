85d88822f94abdd0dd39dd3a72f2cf7442b110b0
JGeun <pjk8926@gmail.com>
2022-02-15 01:03:41 +0900

    서울에서 김서방 찾기

---
+++ src/main/java/Programmers/level1/Main.java
@@ -0,0 +1,10 @@
+package Programmers.level1;
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "김서방은 " + Arrays.asList(seoul).indexOf("Kim") + "에 있다";
+        return answer;
+    }
+}
