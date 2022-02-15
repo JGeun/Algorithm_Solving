d5a39499fe4d994ae7ecfa20a603a02c39df9469
JGeun <pjk8926@gmail.com>
2022-02-16 00:40:44 +0900

    문자열 다루기 기본

--- src/main/java/Programmers/level1/문자열 다루기 기본.java
+++ src/main/java/Programmers/level1/문자열 다루기 기본.java
@@ -2,7 +2,14 @@ package Programmers.level1;
 
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-        return answer;
+        if(s.length() == 4 || s.length() == 6){
+            try{
+                Integer.parseInt(s);
+                return true;
+            }catch(NumberFormatException e){
+                return false;
+            }
+        }
+        return false;
     }
 }
