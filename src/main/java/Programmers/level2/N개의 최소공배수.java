0d19e5d553d2a6f0e724c9377e11f15ae5c7b6f5
JGeun <pjk8926@gmail.com>
2022-02-17 03:04:17 +0900

    N개의 최소공배수

---
+++ src/main/java/Programmers/level2/N개의 최소공배수.java
@@ -0,0 +1,17 @@
+package Programmers.level2;
+
+class Solution {
+    public int gcd(int a, int b){
+        if(a % b == 0) return b;
+        return gcd(b, a%b);
+    }
+    public int gcm(int a, int b){
+        int big, small;
+        return a > b ? a*b/gcd(a,b) : a*b/gcd(b,a);
+    }
+    public int solution(int[] arr) {
+        int answer = 1;
+        for(int num : arr) answer = gcm(answer, num);
+        return answer;
+    }
+}
