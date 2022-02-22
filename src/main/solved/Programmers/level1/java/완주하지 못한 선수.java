c577b410f6d664fc5a861fc2db0af47bd0127039
JGeun <pjk8926@gmail.com>
2022-02-22 21:34:26 +0900

    완주하지 못한 선수

---
+++ src/main/solved/Programmers/level1/java/완주하지 못한 선수.java
@@ -0,0 +1,15 @@
+package Programmers.level1.java;
+
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for(int i=0; i<completion.length; i++)
+            if(!participant[i].equals(completion[i]))
+                return participant[i];
+        return participant[participant.length-1];
+    }
+}
