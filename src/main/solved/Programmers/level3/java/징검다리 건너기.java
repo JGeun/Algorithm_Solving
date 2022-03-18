dc5d5fbcb9010c3c4d1e2291ea6dcfdbe148331c
JGeun <pjk8926@gmail.com>
2022-03-18 16:47:59 +0900

    징검다리 건너기

---
+++ src/main/solved/Programmers/level3/java/징검다리 건너기.java
@@ -0,0 +1,42 @@
+package Programmers.level3.java;
+
+class Solution {
+    public int solution(int[] stones, int k) {
+        int answer = 0, max = -1, min = 200000001;
+
+        for(int stone : stones){
+            max = Math.max(max, stone);
+            min = Math.min(min, stone);
+        }
+
+        return binarySearch(stones, k, min, max);
+    }
+
+    private int binarySearch(int[] stones, int k, int low, int high){
+        if(low == high) return low;
+
+        while(low < high){
+            int mid = (low+high)/2;
+
+            if(canCross(stones, k, mid)){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+
+        return low - 1;
+    }
+
+    private boolean canCross(int[] stones, int k, int friends){
+        int passCount = 0;
+
+        for(int stone : stones){
+            if(stone - friends < 0) passCount += 1;
+            else passCount = 0;
+
+            if(passCount == k) return false;
+        }
+        return true;
+    }
+}
