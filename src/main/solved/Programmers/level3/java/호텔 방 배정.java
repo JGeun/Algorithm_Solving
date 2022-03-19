59da1812fcf898d9986eedb1f17d209d4d11269e
JGeun <pjk8926@gmail.com>
2022-03-19 19:37:36 +0900

    호텔 방 배정

---
+++ src/main/solved/Programmers/level3/java/호텔 방 배정.java
@@ -0,0 +1,27 @@
+package Programmers.level3.java;
+
+import java.util.*;
+
+class Solution {
+    private Map<Long, Long> roomMap = new HashMap();
+    public long[] solution(long k, long[] room_number) {
+        long[] answer = new long[room_number.length];
+
+        for(int i=0; i<room_number.length; i++){
+            answer[i] = findEmptyRoom(room_number[i]);
+        }
+
+        return answer;
+    }
+
+    private long findEmptyRoom(long room_number){
+        if(!roomMap.containsKey(room_number)){
+            roomMap.put(room_number, room_number+1);
+            return room_number;
+        }
+
+        long emptyRoom = findEmptyRoom(roomMap.get(room_number));
+        roomMap.put(room_number, emptyRoom);
+        return emptyRoom;
+    }
+}
