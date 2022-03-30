5ca7f5d802ffa37d377c39d7e06fe0025bacaf59
JGeun <pjk8926@gmail.com>
2022-03-30 19:09:56 +0900

    양과 늑대

---
+++ src/main/solved/Programmers/level3/java/양과 늑대.java
@@ -0,0 +1,35 @@
+package Programmers.level3.java;
+
+import java.util.*;
+
+class Solution {
+    private ArrayList<ArrayList<Integer>> edgeList = new ArrayList<>();
+    private int answer = 0;
+    public int solution(int[] info, int[][] edges) {
+        for(int i=0; i<info.length; i++)
+            edgeList.add(new ArrayList());
+        for(int i=0; i<edges.length; i++)
+            edgeList.get(edges[i][0]).add(edges[i][1]);
+
+        ArrayList<Integer> restEdges = new ArrayList();
+        restEdges.add(0);
+        findSheep(info, restEdges, 0, 0);
+
+        return answer;
+    }
+
+    private void findSheep(int[] info, ArrayList<Integer> restEdges, int sheep, int fox){
+        if(answer != 0 && sheep <= fox) return;
+        answer = Math.max(answer, sheep);
+
+        for(int i=0; i<restEdges.size(); i++){
+            ArrayList<Integer> tempEdges = (ArrayList<Integer>)restEdges.clone();
+            int pos = tempEdges.remove(i);
+            for(int j=0; j<edgeList.get(pos).size(); j++)
+                tempEdges.add(edgeList.get(pos).get(j));
+
+            if(info[pos] == 0) findSheep(info, tempEdges, sheep+1, fox);
+            else findSheep(info, tempEdges, sheep, fox+1);
+        }
+    }
+}
