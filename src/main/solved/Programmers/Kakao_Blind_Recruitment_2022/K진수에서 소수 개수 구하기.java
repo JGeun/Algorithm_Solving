4ad8ff0ba6a1904a8e607e25c439692fbf72f124
JGeun <pjk8926@gmail.com>
2022-04-16 23:54:04 +0900

    K진수에서 소수 개수 구하기

---
+++ src/main/solved/Programmers/Kakao_Blind_Recruitment_2022/K진수에서 소수 개수 구하기.java
@@ -0,0 +1,36 @@
+package Programmers.Kakao_Blind_Recruitment_2022;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+
+        String[] nums = conversion(n, k).split("0");
+        for(String num : nums){
+            try{
+                if(isPrime(Long.parseLong(num))) answer+=1;
+            }catch(NumberFormatException e){
+                continue;
+            }
+        }
+        return answer;
+    }
+    public boolean isPrime(long num){
+        if(num < 2) return false;
+
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num%i==0) return false;
+        }
+        return true;
+    }
+
+    public String conversion(int n, int k){
+        StringBuilder sb = new StringBuilder("");
+        while(n!=0){
+            sb.append(n%k);
+            n /= k;
+        }
+        return sb.reverse().toString();
+    }
+}
