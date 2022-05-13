73a2d2926e50f9b78968a4d8fc740555ec12eec6
JGeun <pjk8926@gmail.com>
2022-05-13 20:50:08 +0900

    무한이진트리

---
+++ src/main/solved/BOJ/p2000/BOJ2078.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int left = 0, right = 0;
+
+        while(!(a==1 && b == 1)) {
+            if(a < b) {
+                b -= a;
+                right += 1;
+            } else{
+                a -= b;
+                left += 1;
+            }
+        }
+
+        System.out.println(left + " " + right);
+    }
+}
