ebf4486e3d4d0037a4eb6f19d9d0b8c1f0bdd867
JGeun <pjk8926@gmail.com>
2022-08-23 00:39:58 +0900

    16진수

---
+++ src/main/solved/BOJ/p1500/BOJ1550.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine(), 16));
+    }
+}
---
+++ src/main/solved/BOJ/p1500/BOJ1550.kt
@@ -0,0 +1,5 @@
+import java.util.*
+
+fun main() {
+    print(readLine()!!.toInt(16))
+}
