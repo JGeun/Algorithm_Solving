9557334ecea0aa973a913404171111fb94c7629c
JGeun <pjk8926@gmail.com>
2022-11-21 16:54:54 +0900

    N과 M (12)

---
+++ src/main/solved/BOJ/p15600/BOJ15666.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int N, M;
+    private static int[] nums;
+    private static LinkedHashSet<String> ans = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        findSequence(new int[M], 0, 0);
+        ans.forEach(System.out::println);
+    }
+
+    private static void findSequence(int[] order, int cnt, int prev) {
+        if (cnt == M) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < M; i++) {
+                sb.append(order[i]).append(' ');
+            }
+            ans.add(sb.toString());
+            return;
+        }
+
+        for (int i = prev; i < N; i++) {
+            order[cnt] = nums[i];
+            findSequence(order, cnt + 1, i);
+        }
+    }
+}
