cd7b8f66a1a025a6d0a7398b751d6642445272dc
JGeun <pjk8926@gmail.com>
2023-06-07 00:42:48 +0900

    거짓말

--- src/main/solved/BOJ/p1000/BOJ1043.java
+++ src/main/solved/BOJ/p1000/BOJ1043.java
@@ -3,44 +3,108 @@ import java.util.*;
 
 class Main {
 
-    private static int N, ans = Integer.MAX_VALUE;
-    private static int[][] dist;
+    private static int N, M;
+    private static int[] parent;
+    private static boolean[] knowArr;
+    private static List<List<Integer>> parties = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        input();
+        solve();
+    }
 
-        N = Integer.parseInt(br.readLine());
-        dist = new int[N][N];
+    private static void solve() {
 
-        for (int i=0; i<N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j=0; j<N; j++) {
-                dist[i][j] = Integer.parseInt(st.nextToken());
+        // parent[i] != i 인 경우 진실을 알고 있다.
+        boolean[] visited = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            if (!knowArr[i] || visited[i]) continue;
+            int root = find(i);
+
+            for (int j = 1; j <= N; j++) {
+                if (root != find(j)) continue;
+
+                knowArr[j] = true;
+                visited[j] = true;
             }
         }
 
-        for (int i=0; i<N; i++) {
-            boolean[] visited = new boolean[N];
-            visited[i] = true;
-            findRoute(i, i, visited, 1, 0);
+        //파티에 진실을 아는 사람이 있는지 체크
+        boolean[] partyLieCheck = new boolean[M];
+        Arrays.fill(partyLieCheck, true);
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (!knowArr[j]) continue;
+                if (!parties.get(i).contains(j)) continue;
+                partyLieCheck[i] = false;
+            }
+        }
+
+        // 전체 순회하면서 거짓을 들은 파티 체크
+        int ans = 0;
+        for (int i = 0; i < M; i++) {
+            if (!partyLieCheck[i]) continue;
+            ans += 1;
         }
 
         System.out.println(ans);
     }
 
-    private static void findRoute(int start, int pos, boolean[] visited, int cnt, int sum) {
-        if (cnt == N) {
-            if (dist[pos][start] != 0) {
-                ans = Math.min(ans, sum+dist[pos][start]);
-            }
-            return;
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
         }
 
-        for (int i=0; i<N; i++) {
-            if (visited[i] || dist[pos][i] == 0) continue;
-            visited[i] = true;
-            findRoute(start, i, visited, cnt+1, sum+dist[pos][i]);
-            visited[i] = false;
+        // 아는 사람들
+        knowArr = new boolean[N + 1];
+        st = new StringTokenizer(br.readLine());
+        int size = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < size; i++) {
+            knowArr[Integer.parseInt(st.nextToken())] = true;
         }
+
+        // 파티 목록 저장
+        for (int i = 0; i < M; i++) parties.add(new ArrayList<>());
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            size = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < size; j++) {
+                parties.get(i).add(Integer.parseInt(st.nextToken()));
+
+                //같이 파티에 참가한 사람들 연결
+                if (j == 0) continue;
+                int nowIdx = parties.get(i).get(j);
+                int prevIdx = parties.get(i).get(j - 1);
+
+                union(prevIdx, nowIdx);
+            }
+        }
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        return find(parent[x]);
+    }
+
+    public static boolean union(int x, int y) {
+        x = find(x); //x의 부모노드 찾기
+        y = find(y); //y의 부모노드 찾기
+
+        // 이미 같은 그래프에 속해있을 때 false 반환
+        if (x == y) return false;
+
+        if (x <= y) parent[y] = x;
+        else parent[x] = y;
+        return true;
     }
 }
